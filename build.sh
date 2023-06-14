e6b8c0e8a2d7231842b814d7c01197004561a6d2
Tim Gymnich <tgymnich@icloud.com>
2023-06-15 01:20:39 +0200

    fix build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LLVM_VERSIONS=( 9 10 11 12 13 14 15 )
+LLVM_VERSIONS=( 9 10 11 12 13 14 15 16 )
 UBUNTU_VERSIONS=( 20 )
 
 for LLVM_VERSION in "${LLVM_VERSIONS[@]}"
@@ -8,6 +8,6 @@ do
     for UBUNTU_VERSION in "${UBUNTU_VERSIONS[@]}"
     do
         echo "Building LLVM $LLVM_VERSION on Ubuntu $UBUNTU_VERSION"
-	    docker buildx build . --push --platform linux/arm64,linux/amd64 --tag ghcr.io/enzymead/llvm-docker/ubuntu-$UBUNTU_VERSION-llvm-$LLVM_VERSION:latest --build-arg UBUNTU_VERSION=$UBUNTU_VERSION.04 --build-arg LLVM_VERSION=$LLVM_VERSION 
+	    docker build . --tag ghcr.io/enzymead/llvm-docker/ubuntu-$UBUNTU_VERSION-llvm-$LLVM_VERSION:latest --build-arg UBUNTU_VERSION=$UBUNTU_VERSION.04 --build-arg LLVM_VERSION=$LLVM_VERSION 
     done
 done
